a137231ba7235f22f5ee027f6303e666440056c3
Eddie Arnold <eddiearnoldz@me.com>
2022-04-18 23:53:52 +0100

    first artist draft complete

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -3,7 +3,6 @@ import Mulimba from "./artists/Mulimba"
 const Artists = () => {
 return (
   <>
-    <h1 className="text-xl font-bold underline text-white m-10">Artists</h1>
     <Mulimba/>
   </>
  
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,18 @@ import SocialFollow from "./SocialMedia"
 const Footer = () => {
   return (
     <>
-  <footer className="flex text-center ">
-    <ul className="md:flex flex-wrap text-center
-    md:justify-center  text-white inset-x-0 space-x-5 bottom-5 
-    xl:text-base sm:text-sm text-xs">
-        <li><p>9 Pockets Records</p></li>
-        <li><p>joey@9pockets.co.uk</p></li>
-        <li><p>+44 7772703882</p></li>
-        <li><a href="https://goo.gl/maps/mzCTwWnos4HjF3ap9"><p>📍 London Fields, E8 3SE</p></a></li>
-        <li><SocialFollow/></li>
+  <div className="">
+    <ul className="">
+        <div className="text-center text-sm">
+          <li><p>9 Pockets Records</p></li>
+          <li><p>joey@9pockets.co.uk</p></li>
+          <li><p>+44 7772703882</p></li>
+          <li><a href="https://goo.gl/maps/mzCTwWnos4HjF3ap9"><p>📍 London Fields, E8 3SE</p></a></li>
+          <li><SocialFollow/></li>
+        </div>
+        
     </ul>
-</footer>
+</div>
     
     </>
   )
