7f177586226839324aca522365d095657998f3bd
Eddie Arnold <eddiearnoldz@me.com>
2022-04-18 23:07:39 +0100

    rough template for artist needs styling

--- src/components/artists/Mulimba.js
+++ src/components/artists/Mulimba.js
@@ -5,9 +5,9 @@ const Mulimba = () => {
         <div className="p-6 max-w-2xl content-center mt-20">
         <div className="grid grid-cols-3 gap-5">
           <div className="flex flex-col col-span-3">
-            <img className="" src="/images/MULIMBA PRESS SHOTS/MULIMBA PRESS (landscape).jpg" alt="pressImg"/>
+            <img className="rounded" src="/images/MULIMBA PRESS SHOTS/MULIMBA PRESS (landscape).jpg" alt="pressImg"/>
           </div>
-          <div className="flex flex-col col-span-3">
+          <div className="flex flex-col col-span-3 text-center">
             <p className="text-white">
             Since first emerging on the scene as a solo artist in 2019, 
             Mulimba (aka Ash) has been developing both a sound and an unpredictability that 
@@ -19,12 +19,24 @@ const Mulimba = () => {
             and the excitement is palpable </p>
           </div>
           <div className="flex flex-col col-span-3">
-            <iframe title="Mulimba" src="https://open.spotify.com/embed/artist/6qXDwwXdqZp0R4MzTSn7jR?utm_source=generator" width="100%" height="280" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            <iframe className="rounded" title="Mulimba" src="https://open.spotify.com/embed/artist/6qXDwwXdqZp0R4MzTSn7jR?utm_source=generator" width="100%" height="280" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
           </div>
-          <div>
-            <image></image>
-            <video></video>
-          </div>
+  
+            <div className="grid grid-span-1">
+            <img src="/images/MULIMBA PRESS SHOTS/Press Shot 01.jpg" alt=""/>
+            </div>
+            <div className="grid grid-span-1">
+            <img src="/images/9P ARTWORK/H.I.G. FINAL ARTWORK.jpg" alt=""/>
+            </div>
+            <div className="grid grid-span-1">
+            <img src="/images/MULIMBA PRESS SHOTS/Press Shot 03.jpg" alt=""/>
+            </div>
+              <div className="flex flex-col col-span-3">
+                <iframe className="rounded"  width="100%" height="315" src="https://www.youtube.com/embed/BZsf2SvUiOY" title="Chasing Gold" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+              </div>
+              <div className="flex flex-col col-span-3">
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/IdMyOcQR338" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+              </div>
           </div>
         </div>
       </div>
@@ -32,3 +44,4 @@ const Mulimba = () => {
   )
 }
 export default Mulimba
+
