23d4ef45b267210521963dd620cd0ed19d50d2a9
Eddie Arnold <eddiearnoldz@me.com>
2022-04-18 20:06:53 +0100

    changed insta png

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,7 +1,7 @@
 const SocialFollow = () => {
   return (
     <div className="flex space-x-5 hover:scale-110 duration-300 md:visible invisible">
-       <a href="https://www.instagram.com/9pocketsmusic/?hl=en-gb"><img className="h-8 w-8" src='/images/SocialMedia/instagram_media_online_social_icon.png' alt=""/></a>
+       <a href="https://www.instagram.com/9pocketsmusic/?hl=en-gb"><img className="h-8 w-8" src='/images/insta.png' alt=""/></a>
     </div>
   );
 }
