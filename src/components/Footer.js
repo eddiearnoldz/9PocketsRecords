b4e6efcb0e3450c5b2881aa2433c10d4b4d5bbad
Eddie Arnold <eddiearnoldz@me.com>
2022-04-19 22:55:48 +0100

    footer staying put and removed social media component for now. To be added and styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,14 @@
-import SocialFollow from "./SocialMedia"
-
 const Footer = () => {
   
   return (
     <>
-      <footer className="static content-center">
-          <ul>
-              <div className="place-content-center text-sm md:flex block ">
-                <li className="px-2"><p>9 Pockets Records</p></li>
-                <li className="px-2"><p>joey@9pockets.co.uk</p></li>
-                <li className="px-2"><p>+44 7772703882</p></li>
-                <li className="px-2"><a href="https://goo.gl/maps/mzCTwWnos4HjF3ap9"><p>📍 London Fields, E8 3SE</p></a></li>
-                <li className="px-2"><SocialFollow/></li>
-              </div>
-          </ul>
+      <footer className="static content-center ">
+        <ul className="md:place-content-center justify-center text-center md:text-sm text-xs md:flex grid">
+          <li className="px-2"><p>9 Pockets Records</p></li>
+          <li className="px-2"><p>joey@9pockets.co.uk</p></li>
+          <li className="px-2"><p>+44 7772703882</p></li>
+          <li className="px-2"><a href="https://goo.gl/maps/mzCTwWnos4HjF3ap9"><p>📍 London Fields, E8 3SE</p></a></li>
+        </ul>
       </footer>
     </>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,14 +29,14 @@ const NavBar = () => {
         md:flex xl:space-x-11 lg:space-x-9
         md:space-x-7 items-center md:bg-transparent bg-black bg-opacity-90 rounded-md
         md:w-auto w-4/12 text-white
-        md:space-y-0 sm:space-y-5 space-y-7 text-center sm:text-left pr-20 p-8 transition-right ease-in duration-500`
+        md:space-y-0 sm:space-y-5 space-y-7 text-left pr-20 p-8 transition-right ease-in duration-500`
         }
       >
         <div className="static ">
           <li className="hover:scale-110  duration-300">
             <button className="hover:text-red-700" onClick={()=> setIsOpen(!isOpen)}>Artists</button>
               {isOpen && (
-                <div className="rounded-lg mt-5 md:absolute bg-black opacity-80">
+                <div className="rounded-lg w-50 mt-2 md:absolute bg-black opacity-80 text-yellow-500">
                   <li className="block p-1 hover:scale-110 duration-300"><Link to="/artists/mulimba" onClick={() => {setShowNav(!showNav); setIsOpen(!isOpen)}}>Mulimba</Link></li>
                   <li className="block p-1 hover:scale-110 duration-300"><Link to="/artists/leap" onClick={() => {setShowNav(!showNav); setIsOpen(!isOpen)}}>Leap</Link></li>
                   <li className="block p-1 hover:scale-110 duration-300"><Link to="/artists/neckandneck" onClick={() => {setShowNav(!showNav); setIsOpen(!isOpen)}}>N&N</Link></li>
