23feb9914ceb7a6be9f4e661afe5caec1078eca4
Eddie Arnold <eddiearnoldz@me.com>
2022-04-20 09:01:38 +0100

    played around with loading screen but not neccesary

--- src/pages/Acts/Leap.js
+++ src/pages/Acts/Leap.js
@@ -1,29 +1,32 @@
 import Act from "../../components/Act";
 
 const Leap = () => {
+
   return (
-    <Act 
-    bandLogo="/images/LEAP/LEAP_red3.png" 
-    mainImage="/images/LEAP/LEAP Landscape.jpg"
-    bio="There's no point trying to understand Jacky Balfour Scott. 
-    Sure, you can piece his story together: 
-    born in Edinburgh, raised in Cape Town, music college in Brighton etc etc,
-     but you'll never really know what he's going to do next. 
-     Even those closest to him that have witnessed every high 
-     and low don't pretend to know what's going on in the kid's head. 
-     It's easier to sit back and just ride the rollercoaster. 
-     Because with Jacky, whether it's tens of thousands of people cheering his name on some of the world's biggest stages, 
-     or board games in the padded walls of the Balcarres Psychiatric Ward, one thing is always certain: 
-     you're gonna be shocked, in one way or another."
-    spotifyName="Leap"
-    spotifyData="https://open.spotify.com/embed/artist/538iX6YCTybcgzsrjDTrFi?utm_source=generator&theme=0"
-    image1="/images/LEAP/ENERGIES FINAL ARTWORK.jpeg"
-    image2="/images/LEAP/BLOOD & WINE_Artwork_Final.jpg"
-    image3="/images/LEAP/WTSG ARTWORK.jpg"
-    video1="HgYJJliMq8A?modestbranding=1&rel=0"
-    video2="_By2Tj-Q9qc?modestbranding=1&rel=0"
-    band="LEAP"
-    />
+    <>
+      <Act 
+      bandLogo="/images/LEAP/LEAP_red3.png" 
+      mainImage="/images/LEAP/LEAP Landscape.jpg"
+      bio="There's no point trying to understand Jacky Balfour Scott. 
+        Sure, you can piece his story together: 
+        born in Edinburgh, raised in Cape Town, music college in Brighton etc etc,
+        but you'll never really know what he's going to do next. 
+        Even those closest to him that have witnessed every high 
+        and low don't pretend to know what's going on in the kid's head. 
+        It's easier to sit back and just ride the rollercoaster. 
+        Because with Jacky, whether it's tens of thousands of people cheering his name on some of the world's biggest stages, 
+        or board games in the padded walls of the Balcarres Psychiatric Ward, one thing is always certain: 
+        you're gonna be shocked, in one way or another."
+      spotifyName="Leap"
+      spotifyData="https://open.spotify.com/embed/artist/538iX6YCTybcgzsrjDTrFi?utm_source=generator&theme=0"
+      image1="/images/LEAP/ENERGIES FINAL ARTWORK.jpeg"
+      image2="/images/LEAP/BLOOD & WINE_Artwork_Final.jpg"
+      image3="/images/LEAP/WTSG ARTWORK.jpg"
+      video1="HgYJJliMq8A?modestbranding=1&rel=0"
+      video2="_By2Tj-Q9qc?modestbranding=1&rel=0"
+      band="LEAP"
+      />
+    </>
   )
 }
 export default Leap
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,21 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
 
+  
   return (
     <>
       <div className="grid h-full">
-      <div classname="p-6 content-center">
-        <div className="sm:grid grid-cols-3 m-5">
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/BLOOD & WINE_Artwork_Final.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/MUL CG FINAL.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/N&N_CIWYL_Artwork_FIX.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/MULIMBA_BID_FINAL.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/N&N_BL_AW_FINAL.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/WTSG ARTWORK.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/NLTD ARTWORK.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/H.I.G. FINAL ARTWORK.jpg" alt=""></img></Link></div>
-          <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/Potions_AW_Final.JPG" alt=""></img></Link></div>
+        <div classname="p-6 content-center">
+          <div className="sm:grid grid-cols-3 m-5">
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/BLOOD & WINE_Artwork_Final.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/MUL CG FINAL.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/N&N_CIWYL_Artwork_FIX.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/MULIMBA_BID_FINAL.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/N&N_BL_AW_FINAL.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/WTSG ARTWORK.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/NLTD ARTWORK.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/H.I.G. FINAL ARTWORK.jpg" alt=""></img></Link></div>
+            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/Potions_AW_Final.JPG" alt=""></img></Link></div>
           </div>
         </div>
       </div>
