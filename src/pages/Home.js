813101a01633be3266492783ca9f96f9d71f3b44
Eddie Arnold <eddiearnoldz@me.com>
2022-04-20 11:24:16 +0100

    home page images ordered

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,11 @@ const Home = () => {
   
   return (
     <>
-      <div className="grid h-full content-center justify-center">
-          <div className="md:grid grid-cols-3 m-5 max-w-3xl max-h-72">
-            <div className=" hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/mulimba release post.jpg" alt=""></img></Link></div>
-            <div className="hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/LEAP 01.jpg" alt=""></img></Link></div>
-            <div className="hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/AfterlightImage.jpg" alt=""></img></Link></div>
+      <div className="grid h-full justify-center">
+          <div className="md:grid grid-cols-3 m-5 max-w-6xl max-h-72">
+            <div className="hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/mulimba'><img className="rounded-md" src="/images/MULIMBA/mulimba release post.jpg" alt=""></img></Link></div>
+            <div className="hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/leap'><img className="rounded-md" src="/images/LEAP/LEAP 01.jpg" alt=""></img></Link></div>
+            <div className="hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/neckandneck'><img className="rounded-md" src="/images/N&N/AfterlightImage.jpg" alt=""></img></Link></div>
           </div>
       </div>
     </> 
