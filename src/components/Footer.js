4bbcc157342b3f58d609994a1066fbd3732912da
Eddie Arnold <eddiearnoldz@me.com>
2022-04-20 12:47:55 +0100

    centered overlay title on home images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,8 @@ const Footer = () => {
   
   return (
     <>
-      <footer className="static content-center ">
-        <ul className="md:place-content-center justify-center text-center md:text-sm text-xs md:flex grid">
-          <li className="px-2"><p>9 Pockets Records</p></li>
-          <li className="px-2"><p>joey@9pockets.co.uk</p></li>
-          <li className="px-2"><p>+44 7772703882</p></li>
-          <li className="px-2"><a href="https://goo.gl/maps/mzCTwWnos4HjF3ap9"><p>📍 London Fields, E8 3SE</p></a></li>
-        </ul>
+      <footer className=" text-center static ">
+       <a href="https://goo.gl/maps/mzCTwWnos4HjF3ap9"><p>📍 London Fields, E8 3SE</p></a>
       </footer>
     </>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,24 +8,22 @@ const Home = () => {
       <div className="grid h-full justify-center">
           <div className="md:grid grid-cols-3 m-5 max-w-6xl max-h-72">
             <div className="hover:scale-105 duration-300 p-3 object-fit group  text-xl">
-           
               <Link to='/artists/mulimba'>
                 <img className="rounded-md" src="/images/MULIMBA/mulimba release post.jpg" alt="MULIMBA"/>
-                <div className=" invisible group-hover:visible text-center relative bottom-20">MULIMBA</div>
+                <div className=" invisible group-hover:visible text-center relative bottom-1/2">MULIMBA</div>
               </Link>
             </div>
             <div className="hover:scale-105 duration-300 p-3 object-fit group  text-xl">
               <Link to='/artists/leap'>
                 <img className="rounded-md" src="/images/LEAP/LEAP 01.jpg" alt="LEAP"/>
-                <div className=" invisible group-hover:visible text-center relative bottom-20">LEAP</div>
+                <div className=" invisible group-hover:visible text-center relative bottom-1/2">LEAP</div>
               </Link>
             </div>
             <div className="hover:scale-105 duration-300 p-3 object-fit group text-xl">
               <Link to='/artists/neckandneck'>
                 <img className="rounded-md" src="/images/N&N/AfterlightImage.jpg" alt="NECK&NECK"/>
-                <div className=" invisible group-hover:visible duration-300 text-center relative bottom-20 text-black">NECK&NECK</div>
+                <div className=" invisible group-hover:visible duration-300 text-center relative bottom-1/2 text-black">NECK&NECK</div>
               </Link>
-              
             </div>
           </div>
       </div>
