97cd8b766a71f3f127117d913a39e33fd55c524c
Eddie Arnold <eddiearnoldz@me.com>
2022-04-20 11:18:35 +0100

    home page images ordered

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,12 @@ const Home = () => {
   
   return (
     <>
-      <div className="grid h-full">
-        <div classname="p-6 content-center">
-          <div className="sm:grid grid-cols-3 m-5">
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/BLOOD & WINE_Artwork_Final.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/MUL CG FINAL.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/N&N_CIWYL_Artwork_FIX.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/MULIMBA_BID_FINAL.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/N&N_BL_AW_FINAL.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/WTSG ARTWORK.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/NLTD ARTWORK.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/H.I.G. FINAL ARTWORK.jpg" alt=""></img></Link></div>
-            <div className="flex flex-col hover:scale-105 duration-300 p-3"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/Potions_AW_Final.JPG" alt=""></img></Link></div>
+      <div className="grid h-full content-center justify-center">
+          <div className="md:grid grid-cols-3 m-5 max-w-3xl max-h-72">
+            <div className=" hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/mulimba'><img className="rounded" src="/images/MULIMBA/mulimba release post.jpg" alt=""></img></Link></div>
+            <div className="hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/leap'><img className="rounded" src="/images/LEAP/LEAP 01.jpg" alt=""></img></Link></div>
+            <div className="hover:scale-105 duration-300 p-3 object-fit"><Link to='/artists/neckandneck'><img className="rounded" src="/images/N&N/AfterlightImage.jpg" alt=""></img></Link></div>
           </div>
-        </div>
       </div>
     </> 
   )
