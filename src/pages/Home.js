b3afde573da48941ccfedd889b69437c7e46b809
Eddie Arnold <eddiearnoldz@me.com>
2022-04-22 12:00:50 +0100

    scale only changes above small screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,19 +6,19 @@ const Home = () => {
     <>
       <div className="grid h-full place-items-center justify-center">
           <div className="grid grid-cols-3 mx-5 max-w-6xl">
-            <div className="hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
+            <div className="sm:hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
               <Link to='/artists/mulimba'>
                 <img className="rounded-md z-1" src="/images/MULIMBA/mulimba release post.jpg" alt="MULIMBA"/>
                 <div className=" sm:invisible group-hover:visible relative py-2 text-3xl font-bold">MULIMBA</div>
               </Link>
             </div>
-            <div className="hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
+            <div className="sm:hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
               <Link to='/artists/leap'>
                 <img className="rounded-md" src="/images/LEAP/LEAP 01.jpg" alt="LEAP"/>
                 <div className=" sm:invisible group-hover:visible relative py-2 text-3xl font-bold">LEAP</div>
               </Link>
             </div>
-            <div className="hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
+            <div className="sm:hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
               <Link to='/artists/neckandneck'>
                 <img className="rounded-md" src="/images/N&N/AfterlightImage.jpg" alt="NECK&NECK"/>
                 <div className=" sm:invisible group-hover:visible relative py-2 text-3xl font-bold">NECK&NECK</div>
@@ -26,8 +26,6 @@ const Home = () => {
             </div>
           </div>
       </div>
-        <br></br>
-        <br></br>
         <br></br>
         <br></br>
     </> 
