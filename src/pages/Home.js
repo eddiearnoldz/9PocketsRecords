833bcab76e39160c9e7d6b3dfbf4cee56571a054
Eddie Arnold <eddiearnoldz@me.com>
2022-04-22 11:57:04 +0100

    band names always visible in sm screens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,23 +9,20 @@ const Home = () => {
             <div className="hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
               <Link to='/artists/mulimba'>
                 <img className="rounded-md z-1" src="/images/MULIMBA/mulimba release post.jpg" alt="MULIMBA"/>
-                <div className=" invisible group-hover:visible relative py-2 text-3xl font-bold">MULIMBA</div>
-              </Link>
-            </div>
-            <div className="hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
-              <Link to='/artists/neckandneck'>
-                <img className="rounded-md" src="/images/N&N/AfterlightImage.jpg" alt="NECK&NECK"/>
-                <div className=" invisible group-hover:visible focus:visible relative py-2 text-3xl font-bold">NECK&NECK</div>
+                <div className=" sm:invisible group-hover:visible relative py-2 text-3xl font-bold">MULIMBA</div>
               </Link>
             </div>
             <div className="hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
               <Link to='/artists/leap'>
                 <img className="rounded-md" src="/images/LEAP/LEAP 01.jpg" alt="LEAP"/>
-                <div className=" invisible group-hover:visible relative py-2 text-3xl font-bold">LEAP</div>
+                <div className=" sm:invisible group-hover:visible relative py-2 text-3xl font-bold">LEAP</div>
               </Link>
             </div>
-            <div>
-
+            <div className="hover:scale-105 sm:col-span-1 col-span-3 duration-500 p-3 object-fit relative group text-center">
+              <Link to='/artists/neckandneck'>
+                <img className="rounded-md" src="/images/N&N/AfterlightImage.jpg" alt="NECK&NECK"/>
+                <div className=" sm:invisible group-hover:visible relative py-2 text-3xl font-bold">NECK&NECK</div>
+              </Link>
             </div>
           </div>
       </div>
